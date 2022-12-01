d06e446bd305c7785b19d7c674066fdb280b514c
psyrj5 <psyrj5@exmail.nottingham.ac.uk>
2022-12-01 22:00:04 +0000

    Improve haskell day1

--- 2022/Day1/haskell/day1.hs
+++ 2022/Day1/haskell/day1.hs
@@ -4,42 +4,38 @@ import System.IO
 import Control.Monad
 import System.Win32 (COORD(x))
 
-testData :: [Integer]
+testData :: [Int]
 testData = [-1,1000,2000,3000,-1,4000,-1,5000,6000,-1,7000,8000,9000,-1,10000]
 
 
-groupFirst :: [Integer] -> [Integer]
+groupFirst :: [Int] -> [Int]
 groupFirst [] = []
 groupFirst (-1:_) = []
 groupFirst (x:xs) = x : (groupFirst xs)
 
 
-groupList :: [Integer] -> [[Integer]]
+groupList :: [Int] -> [[Int]]
 groupList [] = []
-groupList (-1:xs) = [groupFirst xs] ++ groupList xs
-groupList (_:xs) = groupList xs
+groupList (x:xs) = if x == (-1) then [groupFirst xs] ++ groupList xs else groupList xs
 
-
-elfSums :: [Integer] -> [Integer]
+elfSums :: [Int] -> [Int]
 elfSums xs = reverse (sort (map sum (groupList xs)))
 
-topN :: Integer -> [Integer] -> Integer
-topN 0 _ = 0
-topN _ [] = 0
-topN n (x:xs) = x + topN (n-1) xs
+topSum :: Int -> [Int] -> Int
+topSum a = sum . take a
 
-elfTop :: [Integer] -> Integer
-elfTop xs = topN 1 (elfSums xs)
+elfTop :: [Int] -> Int
+elfTop xs = topSum 1 (elfSums xs)
 
 
-elfTop3 :: [Integer] -> Integer
-elfTop3 xs = topN 3 (elfSums xs)
+elfTop3 :: [Int] -> Int
+elfTop3 xs = topSum 3 (elfSums xs)
 
-part1 :: Integer 
-part1 = elfTop testData
+part1 :: [Int] -> Int 
+part1 = elfTop
 
-part2 :: Integer
-part2 = elfTop3 testData
+part2 :: [Int] -> Int
+part2 = elfTop3
 
 
 fileName :: String
@@ -48,9 +44,11 @@ fileName = "haskell_day1_input.txt"
 main :: IO ()
 main = do  
         contents <- readFile fileName
-        let list = map (toInteger . readInt) . words $ contents
-        print (elfTop list)
-        print (elfTop3 list)
+        let list = map (readInt) . words $ contents
+        print "Part 1:"
+        print (part1 list)
+        print "Part 2:"
+        print (part2 list)
 
 readInt :: String -> Int
 readInt = read
